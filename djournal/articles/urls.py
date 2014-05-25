6d8c5d0354dba1935bb30853bc310f34d002fe9b
Shao-Chuan Lee <lsc36x@gmail.com>
2014-05-26 02:04:53 +0800

    article title in url

--- djournal/articles/urls.py
+++ djournal/articles/urls.py
@@ -8,6 +8,6 @@ from articles import views
 urlpatterns = patterns('',
     url(r'^(?P<volume_number>\d*)/?$', views.index, name='index'),
     url(r'^(?P<volume_number>\d*)/category/(?P<category_name>\w+)/$', views.view_category, name='view_category'),
-    url(r'^(?P<volume_number>\d*)/article/(?P<article_id>\d+)$', views.view_article, name='view_article'),
+    url(r'^(?P<volume_number>\d*)/article/(?P<article_id>\d+)-(.*)$', views.view_article, name='view_article'),
     url(r'^volumes$', views.list_volumes, name='list_volumes')
 )
